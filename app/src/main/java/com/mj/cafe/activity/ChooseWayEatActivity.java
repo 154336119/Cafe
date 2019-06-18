d0753ecb8285e740cf718f07ea1b7114b4934eb0
juan <154336119@qq.com>
2019-06-18 23:40:50 +0800

    2个页面

---
+++ app/src/main/java/com/mj/cafe/activity/ChooseWayEatActivity.java
@@ -0,0 +1,15 @@
+package com.mj.cafe.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.mj.cafe.BaseActivity;
+import com.mj.cafe.R;
+
+public class ChooseWayEatActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_way_eat);
+    }
+}
--- app/src/main/java/com/mj/cafe/activity/MainActivity.java
+++ app/src/main/java/com/mj/cafe/activity/MainActivity.java
@@ -2,15 +2,50 @@ package com.mj.cafe.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import com.mj.cafe.ActivityUtil;
 import com.mj.cafe.BaseActivity;
 import com.mj.cafe.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.IvZhongWen)
+    ImageView IvZhongWen;
+    @BindView(R.id.IvHanYu)
+    ImageView IvHanYu;
+    @BindView(R.id.IvYingYu)
+    ImageView IvYingYu;
+    @BindView(R.id.IvLogo)
+    ImageView IvLogo;
+    @BindView(R.id.btn)
+    Button btn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.IvZhongWen, R.id.IvHanYu, R.id.IvYingYu, R.id.btn})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.IvZhongWen:
+                break;
+            case R.id.IvHanYu:
+                break;
+            case R.id.IvYingYu:
+                break;
+            case R.id.btn:
+                ActivityUtil.next(this,ChooseWayEatActivity.class);
+                break;
+        }
     }
 }
